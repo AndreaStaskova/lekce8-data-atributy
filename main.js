e6d9cdd598bb042bb9947dab6cecced124663135
Andrea Staskova <andrea@intiveo.com>
2021-04-27 20:32:21 +0200

    [FEATURE] onclick function for counting kcal

--- main.js
+++ main.js
@@ -9,3 +9,21 @@ která vypíše kalorickou hodnotu potraviny.
 BONUS: počítej a zobrazuj celkový počet sněděných kalorií.
 Tj. při klikání na obrázky se postupně sčítají kalorie.
 */
+
+
+let ovoce = document.querySelector(".ovoce").querySelectorAll("img");
+let vysledek = document.querySelector("#vysledek");
+let pocetKalorii = 0;
+//console.log(ovoce);
+
+ovoce.forEach(element => {
+    
+    element.addEventListener("click", () => {
+        //console.log(element.dataset.kalorie);
+        pocetKalorii += parseInt(element.dataset.kalorie);
+        vysledek.innerHTML = "Snězeno " + pocetKalorii + " kcal";
+    })
+    
+});
+
+
